6fdd0f24eeb62b44a86e5e775e896867258b0d04
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-07-14 11:41:33 +0530

    Time: 66 ms (32.41%), Space: 25.3 MB (12.65%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -6,20 +6,20 @@ public:
         int n = nums.size();
         for(int i=nums.size()-1;i>=0;i--)
 		{
-			st.push(nums[i]);
+			st.push(i);
 
 		}
         
         for(int i = n-1 ; i>=0 ; i--){
-            while(!st.empty() and nums[i]>=st.top())
+            while(!st.empty() and nums[i]>=nums[st.top()])
                 st.pop();
             
             if(st.empty())
                 ans.push_back(-1);
             else
-                ans.push_back(st.top());
+                ans.push_back(nums[st.top()]);
             
-            st.push(nums[i]);
+            st.push(i);
             
         }
         reverse(ans.begin() , ans.end());
