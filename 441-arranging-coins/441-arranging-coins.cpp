8d81e9992a410293b4543c0e17c176e3491beaa6
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-07-16 19:45:32 +0530

    Time: 16 ms (21.12%), Space: 5.8 MB (72.08%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int count = 0 ;
+        for(int i = 1 ; i<=n ; i++){
+            if(i<=n)
+                count++;
+            n = n-i;
+            
+        }
+        return count;
+        
+    }
+};
