9be3b0e00937570eef21071d84c373af7e1bb632
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-07-18 09:36:35 +0530

    Time: 3 ms (59.69%), Space: 8.5 MB (36.45%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,16 +11,26 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root , vector<int> &ans){
-        if(root==NULL)
-            return ;
-        dfs(root->left , ans);
-        dfs(root->right , ans);
-        ans.push_back(root->val);
-    }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        dfs(root , ans);
+        if(root==NULL)
+            return ans;
+        stack<TreeNode*> st1 , st2;
+        st1.push(root);
+        while(!st1.empty()){
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left!=NULL){
+                st1.push(root->left);
+            }
+            if(root->right!=NULL)
+                st1.push(root->right);
+        }
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
         return ans;
         
     }
