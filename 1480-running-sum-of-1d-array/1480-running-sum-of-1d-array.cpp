ba18b8182950187e0864830b455c10fca09eced5
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-07-25 09:35:33 +0530

    Time: 3 ms (79.12%), Space: 8.6 MB (34.90%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        int sum = 0 ;
+        for(int i : nums){
+            sum+=i;
+            ans.push_back(sum);
+        }
+        return ans;
+        
+    }
+};
