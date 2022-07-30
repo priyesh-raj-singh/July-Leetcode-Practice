cb4dafdede563f339ede47d31b9dea7d33f82c59
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-07-31 00:16:07 +0530

    Time: 26 ms (14.87%), Space: 8.4 MB (61.48%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -4,14 +4,14 @@ public:
         int n = nums.size();
         int s = accumulate(nums.begin() , nums.end() , 0);
         
-        vector<vector<bool>> dp(n+1 , vector<bool> (s/2 +1, false));
+        vector<vector<bool>> dp(n+1 , vector<bool> (s+1, false));
         dp[0][0] = true;
         
         for(int i = 0 ; i<=n ; i++)
             dp[i][0] = true;
         
         for(int i = 1 ; i<=n ; i++){
-            for(int j = 1 ; j<=s/2 ; j++){
+            for(int j = 1 ; j<=s ; j++){
                 if(nums[i-1]<=j)
                     dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
                 else
